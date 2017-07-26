13b95121e1b0b98b089973af01d9c8545b956617
BDCraven <bencraven@hotmail.com>
2017-07-26 12:55:33 +0100

    User can create new links

--- app/app.rb
+++ app/app.rb
@@ -8,5 +8,14 @@ get '/links' do
   erb :'links/index'
 end
 
+get '/links/new' do
+  erb :'links/new'
+end
+
+post '/links' do
+  Link.create(url: params[:url], title: params[:title])
+  redirect '/links'
+end
+
 run! if app_file == $0
 end
